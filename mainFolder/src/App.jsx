5675d666f06b1d75cca91a3167c76375e9f56841
Mihir Soni <ms6711534@gmail.com>
2024-02-13 13:42:02 +0530

    scroll to top button

--- mainFolder/src/App.jsx
+++ mainFolder/src/App.jsx
@@ -1,11 +1,13 @@
 import Footer from "./components/footer/Footer"
 import Navbar from "./components/navbar/Navbar"
 import AllRoutes from "./routes/AllRoutes"
+import ScrollToTop from "react-scroll-to-top";
 
 function App() {
 
   return (
     <>
+     <ScrollToTop style={{width:"3rem",height:"3rem",backgroundColor:"#DD0285", borderRadius:"100%", paddingTop:"10px", paddingRight:"10px", paddingBottom:"10px", paddingLeft:"10px"}} smooth color="white" />
       <Navbar/>
       <AllRoutes/>
       <Footer/>
---
+++ mainFolder/src/components/backToTop/BackToTop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./backToTop.css"
+
+const BackToTop = () => {
+  return (
+    <div className='backToTop'>
+      <button><i className='bx bx-chevrons-up'></i></button>
+    </div>
+  )
+}
+
+export default BackToTop
