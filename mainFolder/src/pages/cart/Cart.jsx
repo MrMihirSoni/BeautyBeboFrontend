b1e718b56beeccd0523e773479e5ee629793b61f
Mihir Soni <ms6711534@gmail.com>
2024-04-08 17:18:37 +0530

    page refresser

--- mainFolder/src/pages/cart/Cart.jsx
+++ mainFolder/src/pages/cart/Cart.jsx
@@ -6,49 +6,58 @@ import { AuthContext } from "../../contexts/AuthContext";
 
 const Cart = () => {
   const { data, count, setCount } = useContext(PriceContext);
+  const [load, setLoad] = useState(false);
   const { auth } = useContext(AuthContext);
 
-  const handleDelete = async (id) => {
+  const handleDelete = (id) => {
     try {
-      const response = await axios.delete(
-        `https://beautybebobackend-production.up.railway.app/cart/delete/${id}`,
-        { withCredentials: true }
-      );
-      alert("Item deleted from your Cart!")
-      setCount(count + 1);
+      setLoad(true);
+      axios
+        .delete(
+          `https://beautybebobackend-production.up.railway.app/cart/delete/${id}`,
+          { withCredentials: true }
+        )
+        .then((result) => {
+          setLoad(false);
+          alert("Item deleted from your Cart!");
+          setCount(count + 1);
+        })
     } catch (error) {
-      console.log(error);
+      setLoad(false)
     }
   };
 
   return (
     <>
-      {auth?
-      data.length > 0? (
-        <div className="cartBody">
-          {data.map((ele) => (
-            <div className="cartProduct" key={ele._id}>
-              <div className="cartProductDetailsDiv">
-                <div className="cartProductImageDiv">
-                  <img src={ele.image} alt="" />
+      {auth ? (
+        data.length > 0 ? (
+          <div className="cartBody">
+            {data.map((ele) => (
+              <div className="cartProduct" key={ele._id}>
+                <div className="cartProductDetailsDiv">
+                  <div className="cartProductImageDiv">
+                    <img src={ele.image} alt="" />
+                  </div>
+                  <div className="cartProductDetails">
+                    <h3>{ele.title}</h3>
+                    <p>₹{ele.price}</p>
+                  </div>
                 </div>
-                <div className="cartProductDetails">
-                  <h3>{ele.title}</h3>
-                  <p>₹{ele.price}</p>
+                <div className="deleteButtonDiv">
+                  <button onClick={() => handleDelete(ele._id)}>Delete</button>
                 </div>
               </div>
-              <div className="deleteButtonDiv">
-                <button onClick={() => handleDelete(ele._id)}>Delete</button>
-              </div>
+            ))}
+            <div className="buyButtonDiv">
+              <button>Buy Now!</button>
             </div>
-          ))}
-          <div className="buyButtonDiv">
-            <button>Buy Now!</button>
           </div>
-        </div>
+        ) : (
+          <div className="emptyCartMsg">Please Add sothing to cart first</div>
+        )
       ) : (
-        <div className="emptyCartMsg">Please Add sothing to cart first</div>
-      ):<div className="emptyCartMsg">Please login first</div>}
+        <div className="emptyCartMsg">Please login first</div>
+      )}
     </>
   );
 };
